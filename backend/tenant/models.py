b64afb8ef2245e104b443699379212574cbd4350
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2025-12-30 14:49:43 +0000

    add docker compose down -v in devcontainer.json

--- backend/tenant/models.py
+++ backend/tenant/models.py
@@ -9,14 +9,12 @@ def alumno_foto_path(instance, filename):
     return os.path.join("alumnos", filename)
 
 
-# Create your models here.
-# class Alumno(models.Model):
-#     nombre = models.CharField(max_length=100)
-#     edad = models.IntegerField()
-#     foto = models.ImageField(upload_to="alumnos/", blank=True, null=True)
-
-
 class Alumno(models.Model):
     nombre = models.CharField(max_length=100)
     edad = models.IntegerField()
-    foto = models.ImageField(upload_to=alumno_foto_path, blank=True, null=True)
+    foto = models.ImageField(
+        upload_to=alumno_foto_path, blank=True, null=True
+    )  # upload_to="alumnos/"
+
+    def __str__(self):
+        return f"{self.nombre}"
