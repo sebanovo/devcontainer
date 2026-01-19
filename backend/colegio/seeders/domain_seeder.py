486c628b379856c7f90ff260667ecc454c2d4c7f
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2026-01-19 21:44:33 +0000

    add .nip.io and .sslip.io in domains seeders

--- backend/colegio/seeders/domain_seeder.py
+++ backend/colegio/seeders/domain_seeder.py
@@ -52,6 +52,18 @@ class DomainSeeder:
                 is_primary=True,
             )
 
+            Domain.objects.create(
+                domain=f"espiritu-santo.{Env.MAIN_SCHEMA_DOMAIN_DOMAIN}.nip.io",
+                tenant=client3,
+                is_primary=False,
+            )
+
+            Domain.objects.create(
+                domain=f"espiritu-santo.{Env.MAIN_SCHEMA_DOMAIN_DOMAIN}.sslip.io",
+                tenant=client3,
+                is_primary=False,
+            )
+
             client4 = Client.objects.get(id=4)
             Domain.objects.create(
                 domain=f"boliviano-americano.{Env.MAIN_SCHEMA_DOMAIN_DOMAIN}",
@@ -59,11 +71,34 @@ class DomainSeeder:
                 is_primary=True,
             )
 
+            Domain.objects.create(
+                domain=f"boliviano-americano.{Env.MAIN_SCHEMA_DOMAIN_DOMAIN}.nip.io",
+                tenant=client4,
+                is_primary=False,
+            )
+
+            Domain.objects.create(
+                domain=f"boliviano-americano.{Env.MAIN_SCHEMA_DOMAIN_DOMAIN}.sslip.io",
+                tenant=client4,
+                is_primary=False,
+            )
+
             client5 = Client.objects.get(id=5)
             Domain.objects.create(
                 domain=f"britanico-santa-cruz.{Env.MAIN_SCHEMA_DOMAIN_DOMAIN}",
                 tenant=client5,
                 is_primary=True,
+            )
+            Domain.objects.create(
+                domain=f"britanico-santa-cruz.{Env.MAIN_SCHEMA_DOMAIN_DOMAIN}.nip.io",
+                tenant=client5,
+                is_primary=False,
+            )
+
+            Domain.objects.create(
+                domain=f"britanico-santa-cruz.{Env.MAIN_SCHEMA_DOMAIN_DOMAIN}.sslip.io",
+                tenant=client5,
+                is_primary=False,
             )
         except Exception as e:
             raise e
