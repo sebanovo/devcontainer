8bb9f8a4b78936f60baab6d2df0834ae9cdebbcc
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2026-01-21 16:21:04 +0000

    add guards and default pages not found and not subdomain Register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,68 +1,33 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Env from './constants/env';
 import LandingPage from './pages/LandingPage';
 import CreateTenant from './pages/CreateTenant';
 import LandingPageTenant from './tenant/pages/LandingPageTenant';
 import LoginPageTenant from './tenant/pages/LoginPageTenant';
 import SignUpPageTenant from './tenant/pages/SignUpPageTenant';
+import TenantGuard from './guards/TenantGuard';
+import { getSubdomain, validateHost } from './utils';
+import NotFound from './pages/fallback/NotFound';
 
-function AppHome() {
-  return (
-    <div>
-      <img src={`${Env.VITE_API_URL}/static/mark.jpg`}></img>
-      <img src={`${Env.VITE_API_URL}/static/mark.jpg`}></img>
-      <h1>{Env.VITE_SYSTEM_NAME}</h1>
-    </div>
-  );
-}
-
-function AdminHome() {
-  return <h1>Hola Aleman</h1>;
-}
-
-export function AppRouter() {
-  return (
-    <Routes>
-      <Route path='/' element={<AppHome />}></Route>
-    </Routes>
-  );
-}
-
-export function AdminRouter() {
-  return (
-    <Routes>
-      <Route path='/' element={<AdminHome />}></Route>
-    </Routes>
-  );
-}
 function App() {
-  // // const CurrentApp = getApp();
-  // // console.log({ app: CurrentApp });
-  // return (
-  //   // <BrowserRouter>
-  //   //   <CurrentApp />
-  //   // </BrowserRouter>
-  //   <BrowserRouter>
-  //     <Routes>
-  //       <Route path='/' element={<LandingPage />} />
-  //       <Route path='/create-tenant' element={<CreateTenant />} />
-  //     </Routes>
-  //   </BrowserRouter>
-  // );
   // Detectar subdominio
-  const host = window.location.hostname; // ejemplo: acme.localhost
-  const subdomain = host.split('.')[0];
+  // const host = window.location.hostname; // ejemplo: acme.localhost
+  const isHost = validateHost(window.location.hostname);
+  if (!isHost) return <NotFound />;
+  const subdomain = getSubdomain(window.location.hostname);
+  const isTenant = !!subdomain;
 
   // Si subdomain es distinto del principal (localhost), cargamos el tenant
-  const isTenant = subdomain && subdomain !== Env.VITE_HOST;
+  // const isTenant = subdomain && subdomain !== Env.VITE_HOST;
   if (isTenant) {
     return (
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LandingPageTenant />} />
-          <Route path='/login' element={<LoginPageTenant />} />
-          <Route path='/signup' element={<SignUpPageTenant />} />
-        </Routes>
+        <TenantGuard>
+          <Routes>
+            <Route path='/' element={<LandingPageTenant />} />
+            <Route path='/login' element={<LoginPageTenant />} />
+            <Route path='/signup' element={<SignUpPageTenant />} />
+          </Routes>
+        </TenantGuard>
       </BrowserRouter>
     );
   }
