453cff84296edfbac497a8dcde490e0ef33ea4da
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2026-01-21 17:02:29 +0000

    add router gropu

--- backend/tenant/routers.py
+++ backend/tenant/routers.py
@@ -2,5 +2,6 @@ from rest_framework import routers
 from .views import *
 
 router = routers.DefaultRouter()
+router.register(r"groups", UserViewSet, "tenant-groups")
 router.register(r"users", UserViewSet, "tenant-users")
 router.register(r"alumnos", AlumnoViewSet, "tenant-alumnos")
