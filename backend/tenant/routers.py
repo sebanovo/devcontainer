cde6063256c4781d57299f80deeb5475226e88eb
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2026-01-22 02:54:53 +0000

    fixed error

--- backend/tenant/routers.py
+++ backend/tenant/routers.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .views import *
 
 router = routers.DefaultRouter()
-router.register(r"groups", UserViewSet, "tenant-groups")
+router.register(r"groups", GroupViewSet, "tenant-groups")
 router.register(r"users", UserViewSet, "tenant-users")
 router.register(r"alumnos", AlumnoViewSet, "tenant-alumnos")
